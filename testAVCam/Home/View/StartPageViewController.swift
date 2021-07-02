25bd7e0a03469deaa0fe2ff6352c514fff08ba4e
shawn <shawn.huang1982@gmail.com>
2021-07-02 15:24:42 +0800

    rx version

--- testAVCam/Home/View/StartPageViewController.swift
+++ testAVCam/Home/View/StartPageViewController.swift
@@ -28,38 +28,40 @@ class StartPageViewController: UIViewController {
     
     private func setupBinidng() {
         takePhotoButton.rx
-            .touchUpInside.bind {
-            [weak self] in
-            guard let self = self else { return }
-            self.checkPermission { (isCanLaunchCamera) in
-                DispatchQueue.main.async {
-                    if isCanLaunchCamera {
-                        self.present(self.cameraViewController, animated: true)
-                    } else {
-                        self.presentAlertView(type: .okAction(message: "請同意權限", handler: { (action) in
-                            //handle redirect to setting app
-                        }))
-                    }
-                }
-            }
-           
-        }.disposed(by: self.disposeBag)
+            .touchUpInside
+            .flatMap{ [unowned self] in self.checkPermission() }
+            .subscribe(onNext: { [weak self] isCanLaunchCamera in
+                self?.launchCamerInCan(isCan: isCanLaunchCamera)
+            }).disposed(by: self.disposeBag)
     }
     
-    private func checkPermission(_ callback: @escaping (Bool) -> Void) {
-        switch AVCaptureDevice.authorizationStatus(for: .video) {
-        case .authorized:
-            callback(true)
-        case .notDetermined:
-            AVCaptureDevice.requestAccess(for: .video, completionHandler: { granted in
-                callback(granted)
-            })
-        default:
-            callback(false)
+    private func checkPermission() -> Observable<Bool> {
+        return Observable.create { observer -> Disposable in
+            switch AVCaptureDevice.authorizationStatus(for: .video) {
+            case .authorized:
+                observer.onNext(true)
+            case .notDetermined:
+                AVCaptureDevice.requestAccess(for: .video, completionHandler: { granted in
+                    observer.onNext(granted)
+                })
+            default:
+                observer.onNext(false)
+            }
+        return Disposables.create()
         }
     }
-
-
+    
+    private func launchCamerInCan(isCan: Bool) {
+        DispatchQueue.main.async {
+            if isCan {
+                self.present(self.cameraViewController, animated: true)
+            } else {
+                self.presentAlertView(type: .okAction(message: "請同意權限", handler: { (action) in
+                    //handle redirect to setting app
+                }))
+            }
+        }
+    }
 }
 
 extension StartPageViewController: CameraViewControllerDelegate {
